11d9f429b5db4ab6e835ae64c36d13a49cb58363
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-08-23 16:31:29 -0300

    configurei o nome da chave estrangeira do banco

--- src/main/java/com/apirest/userdept/entities/User.java
+++ src/main/java/com/apirest/userdept/entities/User.java
@@ -7,10 +7,14 @@ import jakarta.persistence.*;
 
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id ;
     private String name;
     private String email;
 
+    @ManyToOne
+    @JoinColumn(name = "department_id")
     private Department department;
 
     public User(){
