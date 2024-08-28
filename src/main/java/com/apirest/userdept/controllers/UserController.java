f6d4d56a58e81f75ed4fdbce26c580aba7a84876
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-08-28 09:42:46 -0300

    coloquei a anotacion @Getmapping

--- src/main/java/com/apirest/userdept/controllers/UserController.java
+++ src/main/java/com/apirest/userdept/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.apirest.userdept.entities.User;
 import com.apirest.userdept.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class UserController {
     @Autowired
     private UserController repository;
 
+    @GetMapping
     public List<User> findAll() {
         List<User> result = repository.findAll();
         return result;
