d16fe40e22e0998ec23d0ef16b935491a865ea45
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-08-28 09:54:41 -0300

    Corrigi a anotacion UserRepository

--- src/main/java/com/apirest/userdept/controllers/UserController.java
+++ src/main/java/com/apirest/userdept/controllers/UserController.java
@@ -1,21 +1,21 @@
 package com.apirest.userdept.controllers;
 
-import com.apirest.userdept.entities.User;
-import com.apirest.userdept.repositories.UserRepository;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.apirest.userdept.entities.User;
+import com.apirest.userdept.repositories.UserRepository;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
     @Autowired
-    private UserController repository;
+    private UserRepository repository;
 
     @GetMapping
     public List<User> findAll() {
