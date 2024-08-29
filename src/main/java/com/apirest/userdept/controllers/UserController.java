666fcaa4652284e939d8b9cdae58d2e17ca94e95
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-08-29 11:51:15 -0300

    fiz o metodo para salvar

--- src/main/java/com/apirest/userdept/controllers/UserController.java
+++ src/main/java/com/apirest/userdept/controllers/UserController.java
@@ -3,10 +3,7 @@ package com.apirest.userdept.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.apirest.userdept.entities.User;
 import com.apirest.userdept.repositories.UserRepository;
@@ -29,4 +26,11 @@ public class UserController {
         User result = repository.findById(id).get();
         return result;
     }
+
+    @PostMapping
+    public User insert(@RequestBody User user) {
+        User result = repository.save(user);
+        return result;
+    }
+
 }
