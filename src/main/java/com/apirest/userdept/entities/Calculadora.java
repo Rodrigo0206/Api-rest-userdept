a05e87818211e0eff6b96de517444eacbfd9c267
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-09-05 17:32:35 -0300

    Fiz teste para reecrever o codigo e deixei como anotaçao o teste de verificaPar

--- src/main/java/com/apirest/userdept/entities/Calculadora.java
+++ src/main/java/com/apirest/userdept/entities/Calculadora.java
@@ -2,11 +2,10 @@ package com.apirest.userdept.entities;
 
 public class Calculadora {
 
-    public int soma(int x , int y){
-        return x + y;
+    public int soma(int x, int y){
+        return  x + y;
     }
-
-    public int divisao(int x, int y){
-        return x / y;
+    public int divisao(int a, int b){
+        return a/b;
     }
 }
--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -4,11 +4,21 @@ import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
 
+
     @Test
     public void somaDoisNumeros(){
         Calculadora calculadora = new Calculadora();
-        int soma = calculadora.soma(10,20);
-        Assertions.assertEquals(30,soma);
+        int somando = calculadora.soma(10, 20);
+        //System.out.println(somando);
+        Assertions.assertEquals(30, somando);
+
     }
+
+   /* @Test
+    public void verificaSeEPar(){
+        Calculadora calculadora = new Calculadora();
+        boolean verifica = calculadora.verificaPar(4);
+        Assertions.assertTrue(verifica);
+    }*/
 
 }
