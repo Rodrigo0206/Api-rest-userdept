a96e6c6e7309a65d2c07aef7d8acd8993f96ff56
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-09-05 09:45:35 -0300

    Fiz o primeiro teste da classe Calculadora com resultado 30

---
+++ src/test/java/CalculadoraTest.java
@@ -0,0 +1,13 @@
+import com.apirest.userdept.entities.Calculadora;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTest {
+
+    @Test
+    public void somaDoisNumeros(){
+        Calculadora calculadora = new Calculadora();
+        int soma = calculadora.soma(10,20);
+        System.out.println(soma);
+    }
+
+}
