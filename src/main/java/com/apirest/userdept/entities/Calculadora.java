cd63f1d95a052e10bce63881a4f01551704bdd70
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-09-05 09:33:04 -0300

    criei uma classe para iniciar os testes unitarios

---
+++ src/main/java/com/apirest/userdept/entities/Calculadora.java
@@ -0,0 +1,12 @@
+package com.apirest.userdept.entities;
+
+public class Calculadora {
+
+    public int soma(int x , int y){
+        return x + y;
+    }
+
+    public int divisao(int x, int y){
+        return x / y;
+    }
+}
