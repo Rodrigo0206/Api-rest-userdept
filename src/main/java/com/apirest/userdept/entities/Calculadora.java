b775aec5acd95e21336f03b9ce7814262ef33f18
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-09-12 09:53:05 -0300

    fiz o metodo calculo dentro da calculadora

--- src/main/java/com/apirest/userdept/entities/Calculadora.java
+++ src/main/java/com/apirest/userdept/entities/Calculadora.java
@@ -8,4 +8,17 @@ public class Calculadora {
     public int divisao(int a, int b){
         return a/b;
     }
+
+    private static Integer calculo(int x, int y){
+
+        int calc = 0;
+
+        if(x % 2 == 0){
+            calc = x + y;
+        }
+        else{
+            calc = x * y;
+        }
+        return  calc;
+    }
 }
